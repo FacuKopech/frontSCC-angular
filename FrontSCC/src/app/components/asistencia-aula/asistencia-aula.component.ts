81df8cefa57c71038010e01f7ea531b9bcc46945
Facu <facukopech@gmail.com>
2024-11-19 08:43:46 -0300

    added pagination controls to asistencia-aula component

--- FrontSCC/src/app/components/asistencia-aula/asistencia-aula.component.ts
+++ FrontSCC/src/app/components/asistencia-aula/asistencia-aula.component.ts
@@ -11,58 +11,117 @@ import { Router } from '@angular/router';
 })
 export class AsistenciaAulaComponent {
 
-  constructor(private aulaService: AulaService, private location: Location, private router: Router, private datePipe: DatePipe){}
+  constructor(private aulaService: AulaService, private location: Location, private router: Router, private datePipe: DatePipe) { }
   public aula: any;
   public asistencias: any[] = [];
+  filteredAsistencias: any[] = [];
   public asistencia: any
   showErrorAlert = false;
   message: string = '';
   asistenciaHoyTomada = false;
   esPresentes = false;
   openAsistenciaAlumnosPopup = false;
+  filterOptions: any = {
+    date: null,
+  };
+  currentPage: number = 1;
+  itemsPerPage: number = 8;
 
-  ngOnInit(): void{
+  ngOnInit(): void {
     this.aula = history.state.data;
     this.aulaService.ObtenerAsistenciasAula(this.aula.id).subscribe(res => {
       this.message = "";
-      if(res){
+      if (res) {
         this.asistencias = res;
+        this.applyFilter();
         console.log(this.asistencias);
-        if(this.asistencias.length == 0){
+        if (this.asistencias.length == 0) {
           this.message = "Nose registraron asistencias para esta aula";
         }
         var today = new Date();
         const formattedDate = this.datePipe.transform(today, 'yyyy-MM-dd');
-        if(formattedDate){
+        if (formattedDate) {
           for (let index = 0; index < this.asistencias.length; index++) {
             const formattedAusenciaDate = this.datePipe.transform(this.asistencias[index].fechaAsistenciaTomada, 'yyyy-MM-dd')
-            if(formattedDate == formattedAusenciaDate){
+            if (formattedDate == formattedAusenciaDate) {
               this.asistenciaHoyTomada = true;
-            }else{
+            } else {
               this.asistenciaHoyTomada = false;
             }
           }
         }
       }
     },
-    (error:HttpErrorResponse) =>{
-      if(error.status == 404 || error.status == 400){
-        this.showErrorAlert = true;
-      }
+      (error: HttpErrorResponse) => {
+        if (error.status == 404 || error.status == 400) {
+          this.showErrorAlert = true;
+        }
+      });
+  }
+
+  applyFilter() {
+    this.filteredAsistencias = this.asistencias.filter(asistencia => {
+      const date = this.filterOptions.date ? new Date(this.filterOptions.date) : null;
+      const asistenciaDate = new Date(asistencia.fechaAsistenciaTomada);
+      
+      const formatDate = (date: Date) => {
+        const day = String(date.getUTCDate()).padStart(2, '0');
+        const month = String(date.getUTCMonth() + 1).padStart(2, '0');
+        const year = date.getUTCFullYear();
+        return `${day}/${month}/${year}`;
+      };
+  
+      const formattedFilterDate = date ? formatDate(date) : null;
+      const formattedAsistenciaDate = formatDate(asistenciaDate);
+      const matchesDate = !formattedFilterDate || formattedAsistenciaDate === formattedFilterDate;
+      return matchesDate;
     });
   }
 
-  public tomarAsistencia(){
-    this.router.navigate(['/tomar_asistencia'], {state: {data: this.aula}});
+  onFilterChange() {
+    this.applyFilter();
   }
 
-  public verAsistenciaAlumnos(asistencia: any, esPresentes: boolean){
+  clearFilter() {
+    this.filterOptions.date = null;
+    this.applyFilter();
+  }
+
+  public tomarAsistencia() {
+    this.router.navigate(['/tomar_asistencia'], { state: { data: this.aula } });
+  }
+
+  public verAsistenciaAlumnos(asistencia: any, esPresentes: boolean) {
     this.asistencia = asistencia;
     this.esPresentes = esPresentes;
     this.openAsistenciaAlumnosPopup = true;
   }
 
-  public goBack(){
+  public calculateIndices() {
+    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    const endIndex = startIndex + this.itemsPerPage;
+    return { startIndex, endIndex };
+  }
+
+  public getPages(): number[] {
+    const totalPages = Math.ceil(this.asistencias.length / this.itemsPerPage);
+    return Array(totalPages).fill(0).map((_, index) => index + 1);
+  }
+
+  public pageClick(page: number) {
+    this.currentPage = page;
+  }
+
+  public previousPageClick() {
+    this.currentPage = this.currentPage - 1
+  }
+
+  public nextPageClick() {
+    this.currentPage = this.currentPage + 1
+  }
+
+
+  public goBack() {
     this.location.back();
   }
 }

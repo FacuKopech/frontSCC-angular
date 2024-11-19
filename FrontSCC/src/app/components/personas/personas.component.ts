9e6c6f45915777b5d8d0560df0d91cd10146574c
Facu <facukopech@gmail.com>
2024-11-19 08:51:04 -0300

    improvements on personas component

--- FrontSCC/src/app/components/personas/personas.component.ts
+++ FrontSCC/src/app/components/personas/personas.component.ts
@@ -8,9 +8,9 @@ import { PersonaService } from 'src/app/services/personas_services/persona.servi
   templateUrl: './personas.component.html',
   styleUrls: ['./personas.component.css']
 })
-export class PersonasComponent{
+export class PersonasComponent {
 
-  constructor(private personaService: PersonaService, private location: Location){}
+  constructor(private personaService: PersonaService, private location: Location) { }
 
   public personas: any[] = [];
   public persona: any;
@@ -27,59 +27,86 @@ export class PersonasComponent{
   openUsuarioPersonaPopup = false;
   openInstitucionPersonaPopup = false;
   currentPage: number = 1;
-  itemsPerPage: number = 3;
+  itemsPerPage: number = 8;
+  filteredPersonas: any[] = [];
+  filterOptions: any = {
+    nombre: '',
+    dni: '',
+  };
 
-  ngOnInit(): void{
+  ngOnInit(): void {
     this.personaService.ObtenerPersonasSistema().subscribe(res => {
       this.message = "";
-      if(res){
+      if (res) {
         this.personas = res;
-        console.log(this.personas);
-        if(this.personas.length == 0){
+        this.applyFilter();
+        if (this.personas.length == 0) {
           this.message = "Usted aun no ha agregado Personas al sistema";
         }
       }
     });
   }
 
-  public verUsuarioPersona(usuario: any){
+  applyFilter() {
+    this.filteredPersonas = this.personas.filter(persona => {
+      const matchesNombre = persona.nombre.toLowerCase().includes(this.filterOptions.nombre.toLowerCase()) || persona.apellido.toLowerCase().includes(this.filterOptions.nombre.toLowerCase());
+      const matchesDNI = persona.dni.toString().includes(this.filterOptions.dni.toString());
+
+      return matchesNombre && matchesDNI;
+    });
+  }
+
+  onFilterChange() {
+    this.applyFilter();
+  }
+
+  clearFilter(tipoFiltro: string) {
+    if (tipoFiltro == 'nombre') {
+      this.filterOptions.nombre = '';
+    } else if (tipoFiltro == 'dni') {
+      this.filterOptions.dni = '';
+    }
+    this.applyFilter();
+  }
+
+  public verUsuarioPersona(usuario: any) {
     this.openUsuarioPersonaPopup = true;
     this.usuario = usuario;
     console.log(this.usuario);
   }
 
-  public verInstitucionPersona(institucion: any){
+  public verInstitucionPersona(institucion: any) {
     this.openInstitucionPersonaPopup = true;
     this.institucion = institucion;
   }
 
-  public eliminarPersona(persona: any){
+  public eliminarPersona(persona: any) {
     this.openDeletePopup = true;
     this.itemForDelete = "Persona";
     this.persona = persona;
   }
 
-  public editarPersona(persona: any){
+  public editarPersona(persona: any) {
     this.persona = persona;
     this.openEditarPersonaPopup = true;
   }
 
-  public deletePersonaConfirmed(){
+  public deletePersonaConfirmed() {
     this.openDeletePopup = false;
     this.personaService.EliminarPersona(this.persona.id).subscribe(res => {
-      if(res){
+      if (res) {
         this.openSuccessAlert = true;
         this.esEliminarPersona = true;
       }
     },
-    (error:HttpErrorResponse) =>{
-      if(error.status >= 400){
-        this.openErrorAlert = true;
-      }
-    })
+      (error: HttpErrorResponse) => {
+        if (error.status >= 400) {
+          this.openErrorAlert = true;
+        }
+      })
   }
 
-  public cerrarSuccess(){
+  public cerrarSuccess() {
     this.openSuccessAlert = false;
     window.location.reload();
   }
@@ -95,19 +122,19 @@ export class PersonasComponent{
     return Array(totalPages).fill(0).map((_, index) => index + 1);
   }
 
-  public pageClick(page: number){
+  public pageClick(page: number) {
     this.currentPage = page;
   }
 
-  public previousPageClick(){
+  public previousPageClick() {
     this.currentPage = this.currentPage - 1
   }
 
-  public nextPageClick(){
+  public nextPageClick() {
     this.currentPage = this.currentPage + 1
   }
 
-  public goBack(){
+  public goBack() {
     this.location.back();
   }
 }

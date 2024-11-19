c49618409b424f2ac59ed44ec7336c68a6caf4f8
Facu <facukopech@gmail.com>
2024-11-19 08:39:11 -0300

    updated environment url for backend calls

--- FrontSCC/src/app/environments/environment.ts
+++ FrontSCC/src/app/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = { 
-  apiUrl: 'https://localhost:7093'  
+  apiUrl: 'https://localhost:7092'  
 };
 
 /*
